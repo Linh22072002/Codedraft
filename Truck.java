abbfef9bf180f5c9b64443b5fc56af7b75dcaca2
Avrey156 <108403904+Avrey156@users.noreply.github.com>
2023-08-25 13:06:56 +0700

    hi

---
+++ Truck.java
@@ -0,0 +1,17 @@
+class Truck {
+    private String id;
+    private String type;
+
+    public Truck(String id, String type, double capacity) {
+        this.id = id;
+        this.type = type;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
